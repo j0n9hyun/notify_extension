add09cb4b4e2b071396a39c9b95ca10afb8d2197
jonghyun choi <j0n9hyun@kakao.com>
2022-08-06 23:46:17 +0900

    Add Styling & Time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import "./styles.css";
+import "./static/scss/home.scss";
+import { useState, useEffect } from "react";
 
-export default function App() {
+function App() {
+  const [time, setTime] = useState(new Date());
+  const date = new Date();
+  const year = time.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  // // console.log(`date:${date.toLocaleDateString("ko-kr")}`);
+  // // console.log(
+  //   `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분 ${seconds}초`
+  // );
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-    </div>
+    <>
+      <div className="home-wrapper">
+        <div className="home-contents">{time.toLocaleTimeString()}</div>
+      </div>
+    </>
   );
 }
+
+export default App;
